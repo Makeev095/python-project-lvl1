aa4395c703fac854981ed07a5e0f5aa0c922daeb
Дмитрий Макеев <makeev095095@mail.ru>
2022-08-08 22:07:57 +0300

    --all

--- brain_games/even.py
+++ brain_games/even.py
@@ -7,17 +7,29 @@ def is_even():
     name = prompt.string('May I have your name? ')
     print("""Answer "yes" if the number is even, otherwise answer "no".""")
     right_answers = 0
-    right_1 = 'yes'
     while right_answers != 3:
         char = randint(1, 1000)
         print(f"Question: {char}")
-        otvet = prompt.string("Your answer: ")
-        if char % 2 == 0 and otvet == 'yes' or char % 2 != 0 and otvet == 'no':
-            right_answers += 1
-            print("Correct!")
-
-            print(f"""'{otvet}' is wrong answer ;(.\n
-            Correct answer was '{right_1}'.\n
-            Let's try again, {name}""")
-            right_answers = 0
+        answer = prompt.string("Your answer: ")
+        if char % 2 == 0:
+            right = 'yes'
+            if answer == right:
+                right_answers += 1
+                print("Correct!")
+            else:
+                print(f"""'{answer}' is wrong answer ;(.\n
+                Correct answer was {right}.\n
+                Let's try again, {name}""")
+                right_answers = 0
+                
+        elif char % 2 != 0:
+            right = 'no'
+            if answer == right:
+                right_answers += 1
+                print("Correct!")
+            else:
+                print(f"""'{answer}' is wrong answer ;(.\n
+                Correct answer was {right}.\n
+                Let's try again, {name}""")
+                right_answers = 0
     print(f"Congratulations, {name}")

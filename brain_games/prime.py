6f0e5cd87fef3c6c3fed3e939989b38d8f755094
Дмитрий Макеев <makeev095095@mail.ru>
2022-08-09 17:52:49 +0300

    --all

--- brain_games/prime.py
+++ brain_games/prime.py
@@ -24,7 +24,7 @@ def is_prime():
             print("Correct!")
         else:
             print(f"""'{answer}' is wrong answer ;(.Correct answer was '{result}'.
-Let's try again, {name}!""")
+            Let's try again, {name}!""")
             right_answers = 0
             return 0
     print(f"Congratulations, {name}!")

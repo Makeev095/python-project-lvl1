0d041aceb1a91542bcbdcde2a0433c297835fd31
Дмитрий Макеев <makeev095095@mail.ru>
2022-08-10 16:11:16 +0300

    --all

--- brain_games/prime.py
+++ brain_games/prime.py
@@ -9,12 +9,13 @@ def is_prime():
     right_answers = 0
     while right_answers != 3:
         number = randint(1, 100)
-        if number > 1:
-            for i in range(2, int(number/2)+1):
-                if (number % i) == 0:
-                    correct_answer = 'no'
-                else:
-                    correct_answer = 'yes'
+        if number==2 or number==3:
+            correct_answer = 'yes'
+        if number%2==0 or number<2:
+            correct_answer = 'no'
+        for i in range(3, int(number**0.5)+1, 2):
+            if number%i==0:
+                correct_answer = 'no'
 
         print(f"Question: {number}")
         answer = prompt.string("Your answer: ")
@@ -26,5 +27,5 @@ def is_prime():
             print(f"""'{answer}' is wrong answer ;(.Correct answer was '{result}'.
 Let's try again, {name}!""")
             right_answers = 0
-            return 0
+    return 0
     print(f"Congratulations, {name}!")

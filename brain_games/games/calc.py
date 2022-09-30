72b2a9c7d5a905a287e878e7728cda6cd81563e6
Дмитрий Макеев <makeev095095@mail.ru>
2022-09-30 16:15:16 +0300

    --all

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -3,16 +3,9 @@ from random import randint
 from random import choice
 
 
-task = """What is the result of the expression? """
-operand_1 = randint(1, 100)
-operand_2 = randint(1, 100)
-operation = choice('-+*')
-question_calc = f"Question: {operand_1} {operation} {operand_2}"
-
-
 def calc_operations():
     name = prompt.string('May I have your name? ')
-    print(task)
+    print("""What is the result of the expression? """)
     right_answers = 0
     for right_answers in range(3):
         operand_1 = randint(1, 100)
@@ -27,7 +20,7 @@ def calc_operations():
         elif operation == '*':
             result = operand_1 * operand_2
 
-        print(question_calc)
+        print(f"Question: {operand_1} {operation} {operand_2}")
         answer = prompt.string("Your answer: ")
         if int(answer) == result:
             right_answers += 1
@@ -38,18 +31,3 @@ Let's try again, {name}!""")
             right_answers = 0
             return 0
     print(f"Congratulations, {name}!")
-
-
-def calc_description():
-    operand_1 = randint(1, 100)
-    operand_2 = randint(1, 100)
-    operation = choice('-+*')
-    if operation == '+':
-        result = operand_1 + operand_2
-
-    elif operation == '-':
-        result = operand_1 - operand_2
-
-    elif operation == '*':
-        result = operand_1 * operand_2
-    return result

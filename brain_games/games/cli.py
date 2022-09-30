5e92f531fc09e59ad09b69d5a20a2be0ddbc62ff
Дмитрий Макеев <makeev095095@mail.ru>
2022-09-30 23:04:44 +0300

    --all

--- brain_games/games/cli.py
+++ brain_games/games/cli.py
@@ -5,8 +5,3 @@ def welcome_user():
     name = prompt.string('May I have your name? ')
     print(f"Hello, {name}!")
     return name
-
-
-def get_answer():
-    answer = prompt.string("Your answer: ")
-    return answer

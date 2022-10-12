53740fd5884e5496b386bc163f3424cc6d4e53d1
Дмитрий Макеев <makeev095095@mail.ru>
2022-10-12 20:06:20 +0300

    --all

--- brain_games/engine_games.py
+++ brain_games/engine_games.py
@@ -2,22 +2,20 @@ import prompt
 
 
 def run(game):
-    right_answers = 0
+    rounds = 3
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name?')
     print(f"Hello, {name}!")
     question = game.game_question()
     print(question)
-    for right_answers in range(3):
+    for _ in range(rounds):
         res, result = game.discription()
         print(result)
         answer = game.getting_answer()
         if answer == res:
             print("Correct!")
-            right_answers += 1
         else:
             print(f"""'{answer}' is wrong answer ;(.Correct answer was '{res}'.
 Let's try again, {name}!""")
-            right_answers = 0
             return 0
     print(f"Congratulations, {name}!")
